1ad7f4759dc122c5bb0fdb2e8fd9de6c9b6729c7
Ivar H. Krabol <ivarhk@stud.ntnu.no>
2019-04-09 23:02:24 +0200

    Use different strategy for mood weight calculation

---
+++ Assets/Scripts/MoodWeightInOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MoodWeightInOut : MoodWeight
+{
+    public Vector3 entrance;
+    public Vector3 exit;
+    public Vector3 bottomLeft;
+    public float size = 42.0f;
+    public float blendRatio = 0.5f;
+
+    protected override float GetWeight()
+    {
+        var distEntrance = Vector3.Distance(CameraPosition, entrance) / (14f * blendRatio);
+        var distExit = Vector3.Distance(CameraPosition, exit) / (14f * blendRatio);
+        return CameraPosition.x >= bottomLeft.x && CameraPosition.x <= bottomLeft.x + size &&
+               CameraPosition.z >= bottomLeft.z && CameraPosition.z <= bottomLeft.z + size
+            ? .5f * (1f + Mathf.Clamp(Math.Min(distExit, distEntrance), 0f, 1f))
+            : .5f * (1f - Mathf.Clamp(Math.Min(distExit, distEntrance), 0f, 1f));
+    }
+}
